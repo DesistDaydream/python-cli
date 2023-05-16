fb2c14a26257cc46b5a75b7cd609df8689e62d41
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-05-16 17:15:50 +0800

    Update demo.py

--- click/demo.py
+++ click/demo.py
@@ -1,10 +1,10 @@
 import click
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
-@click.version_option(version='1.0.0')
+@click.version_option(version="1.0.0")
 def cli():
     """Repo is a command line tool that showcases how to build complex
         command line interfaces with Click.
@@ -15,39 +15,40 @@ def cli():
 
 
 @cli.command()
-@click.argument('name', default='all', required=True)
+@click.argument("name", default="all", required=True)
 # @click.option('--greeting', default='Hello', help='word to use for the greeting')
 # @click.option('--caps', is_flag=True, help='uppercase the output')
 def hellocmd(name):
-    click.echo(
-        click.style(
-            'I am colored %s and bold' %
-            name,
-            fg='green',
-            bold=True))
+    click.echo(click.style("I am colored %s and bold" % name, fg="green", bold=True))
 
 
 @cli.command()
-@click.option('-t', default='a', required=True,
-              type=click.Choice(['a', 'h']), prompt=True, help='检查磁盘空间,a表示所有空间，h表示空间大于50%')
+@click.option(
+    "-t",
+    default="a",
+    required=True,
+    type=click.Choice(["a", "h"]),
+    prompt=True,
+    help="检查磁盘空间,a表示所有空间，h表示空间大于50%",
+)
 def dfcmd(t):
     """
     检查磁盘空间 dfcmd
     :param t:
     :return:
     """
-    click.echo(click.style('检查磁盘空间', fg='green', bold=True))
+    click.echo(click.style("检查磁盘空间", fg="green", bold=True))
 
 
 @cli.command(context_settings=CONTEXT_SETTINGS)
-@click.argument('x', type=int, required=True)
+@click.argument("x", type=int, required=True)
 def square(x):
     """
     得到x平方 square x
     """
-    click.echo(click.style('x= %s' % x, fg='green', bold=True))
+    click.echo(click.style("x = %s" % x, fg="green", bold=True))
     print(x * x)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
