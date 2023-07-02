657b53bdfd15fbb87bee5ed64c704ac8285b596e
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-07-02 18:31:42 +0800

    update

--- click/hello_world.py
+++ click/hello_world.py
@@ -4,7 +4,7 @@ import click
 @click.command()
 @click.option("--count", default=1, help="Number of greetings.")
 @click.option("--name", prompt="Your name", help="The person to greet.")
-def hello(count, name):
+def hello(count: str, name: str):
     """Simple program that greets NAME for a total of COUNT times."""
     for _ in range(count):
         click.echo(f"Hello, {name}!")
